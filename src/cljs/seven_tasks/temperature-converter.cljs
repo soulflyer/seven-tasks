534e246e4b9f038221665b31cce4c17384c55068
iain <iain@soulflyer.co.uk>
2020-12-08 21:03:58 +0700

    3:21 Task 2, Temperature Converter, working correctly

--- src/cljs/seven_tasks/temperature-converter.cljs
+++ src/cljs/seven_tasks/temperature-converter.cljs
@@ -10,14 +10,27 @@
   (* (/ 5 9) (- farenheit 32)))
 
 (defn page []
-  (let [temperature-centigrade (r/atom 0)]
+  (let [temperature-centigrade (r/atom 0)
+        temperature-farenheit  (r/atom 0)]
     (fn []
       [:span.main
        [:h1 "Task 2, Temperature Converter"]
        [:p "Convert Centigrade to Farenheit and vice versa"]
-       [:input {:type "text"
-                :value @temperature-centigrade
-                :on-change #(reset! temperature-centigrade (-> % .-target .-value))}]
-       [:p @temperature-centigrade " Centigrade"]
-       [:p (c->f @temperature-centigrade) " Farenheit"]
-       [:p [:a {:href (routes/path-for :index)} "Back to the list of tasks"]]])))
+       [:p
+        [:input {:type "text"
+                 :value @temperature-centigrade
+                 :on-change (fn [input-value]
+                              (let [degrees-centigrade (-> input-value .-target .-value)]
+                                ((reset! temperature-centigrade degrees-centigrade)
+                                 (reset! temperature-farenheit (c->f degrees-centigrade)))))}]
+        " Centigrade "]
+       [:p
+        [:input {:type "text"
+                 :value @temperature-farenheit
+                 :on-change (fn [input-value]
+                              (let [degrees-farenheit (-> input-value .-target .-value)]
+                                (reset! temperature-farenheit degrees-farenheit)
+                                (reset! temperature-centigrade (f->c degrees-farenheit))))}]
+        " Farenheit"]
+       [:p
+        [:a {:href (routes/path-for :index)} "Back to the list of tasks"]]])))
